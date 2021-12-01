f83bb3a40cd0168a44e7ccb521dec91f95029572
Rheena Catherine <rheenahmacharia95@gmail.com>
2021-12-01 07:06:05 +0300

    Add files via upload

--- main/java/com/example/covtrack/RegisterPageActivity.java
+++ main/java/com/example/covtrack/RegisterPageActivity.java
@@ -1,20 +1,63 @@
 package com.example.covtrack;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class RegisterPageActivity extends AppCompatActivity {
     private FirebaseAuth mAuth;
+    //    private FirebaseAnalytics mFirebaseAnalytics;
     private static final String TAG = "EmailLogin";
+    EditText regFirstName, regLastName, regEmail, regPhone, regPassword;
+    Button regBtn;
 
-
+    FirebaseDatabase rootNode;
+    DatabaseReference recordsDbRef;
 
         @Override
         protected void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
             setContentView(R.layout.activity_registrationpage);
+
+            regFirstName = findViewById(R.id.editTextTextPersonName);
+            regLastName = findViewById(R.id.editTextTextPersonName2);
+            regEmail = findViewById(R.id.editTextTextEmailAddress);
+            regPhone = findViewById(R.id.editTextPhone);
+            regPassword = findViewById(R.id.editTextTextPassword);
+            regBtn = findViewById(R.id.registerbtn);
+
+            recordsDbRef = FirebaseDatabase.getInstance().getReference().child("Patient");
+
+            //Save data in Firebase on button click
+            regBtn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    insertPatientData();
+                }
+            });
+
+        }
+
+        private void insertPatientData(){
+
+            //Get the values
+            String fname = regFirstName.getText().toString();
+            String lname = regLastName.getText().toString();
+            String email = regEmail.getText().toString();
+            String phone = regPhone.getText().toString();
+            String password = regPassword.getText().toString();
+
+            UserHelperClass helperClass = new UserHelperClass(fname, lname, email, phone, password);
+
+            recordsDbRef.push().setValue(helperClass);
         }
 }
--- main/java/com/example/covtrack/WelcomePageActivity.java
+++ main/java/com/example/covtrack/WelcomePageActivity.java
@@ -9,19 +9,15 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
 import com.google.firebase.analytics.FirebaseAnalytics;
-import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 public class WelcomePageActivity extends AppCompatActivity {
     private FirebaseAuth mAuth;
-    private FirebaseAnalytics mFirebaseAnalytics;
+//    private FirebaseAnalytics mFirebaseAnalytics;
     private static final String TAG = "EmailLogin";
     EditText loginEmail, loginPassword;
     Button loginBtn;
@@ -34,7 +30,7 @@ public class WelcomePageActivity extends AppCompatActivity {
 
         mAuth = FirebaseAuth.getInstance();
         // Obtain the FirebaseAnalytics instance.
-        mFirebaseAnalytics = FirebaseAnalytics.getInstance(this);
+//        mFirebaseAnalytics = FirebaseAnalytics.getInstance(this);
         loginEmail = findViewById(R.id.EmailLoginInput);
         loginPassword = findViewById(R.id.PasswordLoginInput);
         loginBtn = findViewById(R.id.loginButton);
@@ -43,7 +39,7 @@ public class WelcomePageActivity extends AppCompatActivity {
 
         loginBtn.setOnClickListener(view -> {
             String email = loginEmail.getText().toString().trim();
-            String password = loginBtn.getText().toString().trim();
+            String password = loginPassword.getText().toString().trim();
 
             if (TextUtils.isEmpty(email)){
                 loginEmail.setError("Email is required!");
@@ -58,20 +54,18 @@ public class WelcomePageActivity extends AppCompatActivity {
 //            }
 
 
-             mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-                 @Override
-                 public void onComplete(@NonNull Task<AuthResult> task) {
-                     if (task.isSuccessful()){
-                         Log.d(TAG, "loginWithEmail:success");
-                         Toast.makeText(WelcomePageActivity.this, "Log in successful!", Toast.LENGTH_SHORT).show();
-                         FirebaseUser user = mAuth.getCurrentUser();
-                         updateUI(user);
-                     } else {
-                         //If login fails log the error and send toast to user
-                         Log.w(TAG, "LoginWithEmail:failed", task.getException());
-                         Toast.makeText(WelcomePageActivity.this, "Log in failed!", Toast.LENGTH_SHORT).show();
-                         updateUI(null);
-                     }
+             mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(this, task -> {
+                 if (task.isSuccessful()){
+                     Log.d(TAG, "loginWithEmail:success");
+                     Toast.makeText(WelcomePageActivity.this, "Log in successful!", Toast.LENGTH_SHORT).show();
+                     FirebaseUser user = mAuth.getCurrentUser();
+                     updateUI(user);
+                     startActivity(new Intent(getApplicationContext(), PatientsPageActivity.class));
+                 } else {
+                     //If login fails log the error and send toast to user
+                     Log.w(TAG, "LoginWithEmail:failed", task.getException());
+                     Toast.makeText(WelcomePageActivity.this, "Log in failed!", Toast.LENGTH_SHORT).show();
+                     updateUI(null);
                  }
              });
         });
